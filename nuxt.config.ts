eadc4ef4b7c2d347b2f467ab580e2a219a95a92d
zengbotao <2898487084@qq.com>
2024-02-28 12:36:26 +0800

    @vant/nuxt组件的使用与导入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
+/*
+ * @Description: 
+ * @Autor: zengbotao@myhexin.com
+ * @Date: 2024-02-28 10:44:17
+ * @LastEditors: 
+ * @LastEditTime: 2024-02-28 12:35:51
+ */
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules:['@vant/nuxt']
 })
